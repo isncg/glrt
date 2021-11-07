f31afd7d706cb0a6de7cdf6cf791b3aefad8a33a
isncg <isn_cg@163.com>
2021-11-07 17:08:15 +0800

    camera

---
+++ Camera.cpp
@@ -0,0 +1,70 @@
+#include "Camera.h"
+
+void Camera::SetProjectionMatrix(float fovY, float aspect, float zNear, float zFar)
+{
+	mat_proj = Matrix4x4::Perspective(fovY, aspect, zNear, zFar);
+}
+
+void Camera::SetShaderMat4(Shader& shader, const char* name)
+{
+	shader.Set(name, mat_proj * mat_view);
+}
+
+
+void CameraController::Update()
+{
+	FrameUpdate();
+	CalcViewMatrix(camera->mat_view);
+}
+
+void CameraFirstPersonController::OnMouseMove(long dx, long dy, long x, long y)
+{
+#define M_PI 3.14159265358979323846
+	float hp = M_PI / 2.0f - 0.000001f;
+	yall += dx * 0.005f;
+	pitch -= dy * 0.005;
+	if (pitch > hp)
+		pitch = hp;
+	if (pitch < -hp)
+		pitch = -hp;
+}
+
+void CameraFirstPersonController::OnKeyboard(KEYS key, KEYACTION action)
+{
+	if (key == KEYS::KEY_W)
+	{
+		mw = action == KEYACTION::KEYDOWN ? 1 : 0;
+	}
+
+	if (key == KEYS::KEY_A)
+	{
+		ma = action == KEYACTION::KEYDOWN ? 1 : 0;
+	}
+
+	if (key == KEYS::KEY_S)
+	{
+		ms = action == KEYACTION::KEYDOWN ? 1 : 0;
+	}
+
+	if (key == KEYS::KEY_D)
+	{
+		md = action == KEYACTION::KEYDOWN ? 1 : 0;
+	}
+}
+
+void CameraFirstPersonController::FrameUpdate()
+{
+	//Vector3 r{ cos(yall) * cos(pitch), sin(pitch), sin(yall) * cos(pitch) };
+	fw.x = cos(yall) * cos(pitch);
+	fw.y = sin(pitch);
+	fw.z = sin(yall) * cos(pitch);
+	Vector3 rt{ sin(yall), 0, -cos(yall) };
+	focus += -(float)(md - ma) * rt * 20.0f;
+	focus += (float)(mw - ms) * fw * 20.0f;
+}
+
+void CameraFirstPersonController::CalcViewMatrix(Matrix4x4& mat_proj)
+{
+	mat_proj = Matrix4x4::LookAt(focus, focus + fw, Vector3{ 0,1,0 });
+}
+
---
+++ Camera.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Renderer.h"
+#include "Window.h"
+
+enum CAMERA_PROJECTION
+{
+	CAMPROJ_PERSPECTIVE = 0,
+	CAMPROJ_ORTHO = 1
+};
+
+class Camera
+{
+	friend class CameraController;
+	Matrix4x4 mat_view;
+	Matrix4x4 mat_proj;
+public:
+	void SetProjectionMatrix(float fovY, float aspect, float zNear, float zFar);
+	void SetShaderMat4(Shader& shader, const char* name = "_cam");
+};
+
+class CameraController
+{
+	virtual void CalcViewMatrix(Matrix4x4& mat_proj) = 0;
+	virtual void FrameUpdate() = 0;
+public:
+	virtual void OnMouseMove(long dx, long dy, long x, long y) = 0;
+	virtual void OnKeyboard(KEYS key, KEYACTION action) = 0;
+	Camera* camera;
+	void Update();
+};
+
+class CameraFirstPersonController: public CameraController
+{
+	int ma = 0;
+	int md = 0;
+	int mw = 0;
+	int ms = 0;
+	float yall = 0;
+	float pitch = 0;
+	Vector3 focus;
+	Vector3 fw;
+	Vector3 rt;
+	virtual void FrameUpdate() override;
+	virtual void CalcViewMatrix(Matrix4x4& mat_proj) override;
+public:
+	virtual void OnMouseMove(long dx, long dy, long x, long y) override;
+	virtual void OnKeyboard(KEYS key, KEYACTION action) override;
+};

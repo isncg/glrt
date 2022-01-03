a69fb4a6dfac86099c367357c55c32da609c6311
isncg <isn_cg@163.com>
2022-01-04 00:29:05 +0800

    remove old hl1 utils

--- utils/HL/HL.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-// Loading Halflife 1 BPS & WAD to Model & Texture
-#include <wad/WADFile.h>
-#include <bsp30/BSPLoader.h>
-#include "../../include/GLRT.h"
-
-void HLMapLoad(std::string bsp, std::string wad, Model& outputModel, std::map<std::string, Texture>* outputTextures);
